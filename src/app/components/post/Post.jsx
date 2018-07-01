cadd24477b1a4fdf778aa9d01b55e979cad2306b
Igor_Fedyukin <igor_fedyukin@epam.com>
2018-07-01 18:10:38 +0300

    CRA and some fixes

--- src/app/components/post/Post.jsx
+++ src/app/components/post/Post.jsx
@@ -5,10 +5,12 @@ import PostInfo from './PostInfo';
 
 export default class Post extends React.Component {
     render() {
-        return <div className="post">
-            <div className="detele-button">X</div>
-           <PostImg image={this.props.image} alt={this.props.alt}/>
-           <PostInfo buttontext={"More.."} appstate={this.props.appstate} title={this.props.title} description={this.props.description}/>
-        </div>
+        return (
+            <div className="post">
+                <div className="detele-button">X</div>
+                <PostImg image={this.props.image} alt={this.props.alt} />
+                <PostInfo buttontext={"More.."} appstate={this.props.appstate} title={this.props.title} description={this.props.description} />
+            </div>
+        );
     }
 }
